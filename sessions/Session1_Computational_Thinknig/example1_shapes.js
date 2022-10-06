e0bd93af6d1af10b32af03cfb96987e5969c91b1
edipa <64174486+edipa@users.noreply.github.com>
2022-10-06 13:17:36 +0200

    Create example1_shapes.js

---
+++ sessions/Session1_Computational_Thinknig/example1_shapes.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  //red square with white outline
+  stroke(255);
+  fill(255,0,0);
+  rect(0, 0, 100, 90);
+  
+  //blue rectangle with no outline
+  noStroke();
+  fill(0,0,255);
+  rect(100,100,200,100);
+  
+  //white cricle 
+  fill(255);
+  circle(300,300,150)
+  
+}
